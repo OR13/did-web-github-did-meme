20269f91517c11db3939a8cf5637212a65f33de6
Orie Steele <orie@transmute.industries>
2022-03-26 11:06:27 -0500

    update install script

---
+++ scripts/install.sh
@@ -0,0 +1,17 @@
+echo 
+echo "Installing ✨ "$1
+echo 
+
+DID_MEME=$1
+curl -s https://didme.me/api/$DID_MEME | jq '.' > parent.json
+ID=$(cat ./parent.json | jq -r '.didDocument.id | split(":")[2]')
+USERNAME=$(gh repo view --json url | jq '.url | split("/")[3]')
+DID_WEB="did:web:$USERNAME.github.io:memes:$ID"
+mkdir -p ./docs/memes/$ID
+mv ./parent.json ./docs/memes/$ID/parent.json
+cat ./docs/memes/$ID/parent.json | jq '.didDocument' | sed "s/$DID_MEME/$DID_WEB/g"  > ./docs/memes/$ID/did.json
+echo
+echo "1. say buh bye to 🔥 $DID_MEME"
+echo
+echo "2. say wassup to  🌴 $DID_WEB"
+echo
